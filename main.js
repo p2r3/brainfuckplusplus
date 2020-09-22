5404d4360e97a491536e8577c2dd645b2575fdfd
Jacob <p2r3@GuidanceStone.labr.it>
2020-09-22 22:52:27 +0300

    v0.2

--- main.js
+++ main.js
@@ -7,11 +7,16 @@ function setCode(val){
 }
 
 function setOutput(val){
-  return document.getElementById("output").innerHTML = val;
+  return document.getElementById("output").value = val;
+}
+
+function helloworld(){
+  setCode("a = 5 - 3\n\nb = a + 48\n\nb >\n\n\"  Hello, world!");
+  setOutput("[-]++><[->>+<<]>>[-<<+>>>+<]>++++++++++++++++++++++++++++++++++++++++++++++++<<[-]>>[-<<+>>]<<.<[-]++++++++++++++++++++++++++++++++.++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------.------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.--------.+++.------.--------.-------------------------------------------------------------------.[-]");
 }
 
 function getIndex(name){
-  for(var i = 2; i < varNames.length; i++){
+  for(var i = 0; i < varAmount; i++){
     if(name == varNames[i]) return(i);
   }
   alert("Error: Can't get index of " + name + " because it is undefined.");
@@ -31,21 +36,21 @@ function goToIndex(ind){
 }
 
 function fullAddition(ind1, ind2, rez){
-  singleAddition(ind1, 1);
-  singleAddition(ind2, 1);
+  singleAddition(ind1, varAmount + 1);
+  singleAddition(ind2, varAmount + 1);
   clear(rez);
-  singleMove(1, rez);
+  singleMove(varAmount + 1, rez);
 }
 
 function singleAddition(ind1, ind2){
-  singleMove(ind1,0);
-  goToIndex(0);
+  singleMove(ind1, varAmount);
+  goToIndex(varAmount);
   output+="[-";
   goToIndex(ind1);
   output+="+";
   goToIndex(ind2);
   output+="+";
-  goToIndex(0);
+  goToIndex(varAmount);
   output+="]";
 }
 
@@ -69,41 +74,41 @@ function addNumber(ind, num){
 }
 
 function fullAddNumber(ind, num, rez){
-  singleAddition(ind, 1);
-  addNumber(1, num);
+  singleAddition(ind, varAmount + 1);
+  addNumber(varAmount + 1, num);
   clear(rez);
-  singleMove(1, rez);
+  singleMove(varAmount + 1, rez);
 }
 
 function fullSubtraction(ind1, ind2, rez){
-  singleAddition(ind1, 1);
-  singleSubtraction(ind2, 1);
+  singleAddition(ind1, varAmount + 1);
+  singleSubtraction(ind2, varAmount + 1);
   clear(rez);
-  singleMove(1, rez);
+  singleMove(varAmount + 1, rez);
 }
 
 function singleSubtraction(ind1, ind2){
-  singleMove(ind1,0);
-  goToIndex(0);
+  singleMove(ind1, varAmount);
+  goToIndex(varAmount);
   output+="[-";
   goToIndex(ind1);
   output+="+";
   goToIndex(ind2);
   output+="-";
-  goToIndex(0);
+  goToIndex(varAmount);
   output+="]";
 }
 
 function fullSubtNumber(ind, num, rez, invert){
   if(invert){
-    addNumber(1, num);
-    singleSubtraction(ind, 1);
+    addNumber(varAmount + 1, num);
+    singleSubtraction(ind, varAmount + 1);
   } else {
-    singleAddition(ind, 1);
-    subtNumber(1, num);
+    singleAddition(ind, varAmount + 1);
+    subtNumber(varAmount + 1, num);
   }
   clear(rez);
-  singleMove(1, rez);
+  singleMove(varAmount + 1, rez);
 }
 
 function subtNumber(ind, num){
@@ -111,25 +116,78 @@ function subtNumber(ind, num){
   for(var i = 0; i < num; i++) output+="-";
 }
 
+function ifEquals(var1, var2){
+
+  var int1 = parseInt(var1, 10), int2 = parseInt(var2, 10);
+
+  if(isNaN(var1) && isNaN(var2)){
+    singleAddition(getIndex(var1), varAmount + 1);
+    singleAddition(getIndex(var2), varAmount + 2);
+  } else if(isNaN(var2)){
+    addNumber(varAmount + 1, int1);
+    singleAddition(getIndex(var2), varAmount + 2);
+  } else if(isNaN(var1)){
+    singleAddition(getIndex(var1), varAmount + 1);
+    addNumber(varAmount + 2, int2);
+  } else {
+    addNumber(varAmount + 1, int1);
+    addNumber(varAmount + 2, int2);
+  }
+
+  goToIndex(varAmount + 3);
+  output += "+<<[->-<]>[>-<[-]]>[";
+
+  clear(varAmount + 1);
+  clear(varAmount + 2);
+  clear(varAmount + 3);
+
+}
+
 var varNames = [];
+var varAmount = 0;
 var globalIndex = 0;
 var output = "";
 
-varNames[0] = "TEMP1";
-varNames[1] = "TEMP2"
-
 function compile(){
+
+  varNames = new Array();
+  varAmount = 0;
+  globalIndex = 0;
   output = "";
+
   var lines = getCode().split("\n");
   for(var i = 0; i < lines.length; i++){
 
     if(lines[i][0] == "/" && lines[i][1] == "/") continue;
 
+    if(lines[i].indexOf("if ") == 0) {
+
+      var split = lines[i].split(" ");
+
+      if(split[2] == "==") ifEquals(split[1], split[3]);
+
+      continue;
+
+    }
+
+    if(lines[i].indexOf("fi") == 0) {
+
+      goToIndex(varAmount);
+      clear(varAmount);
+      output += "]";
+
+      continue;
+
+    }
+
     if(lines[i][0] == "\"" && lines[i][1] == " ") {
-      
-      clear(0);
+
       var temp = 0;
-      
+      lines[i] += "\n";
+
+      goToIndex(varAmount);
+      clear(varAmount);
+
       for(var j = 2; j < lines[i].length; j++) {
         while(temp != lines[i].charCodeAt(j)){
           if(temp < lines[i].charCodeAt(j)){
@@ -142,20 +200,20 @@ function compile(){
         }
         output += ".";
       }
-      
-      clear(0);
-      
+
+      clear(varAmount);
+
       continue;
-      
+
     }
 
     if(lines[i].indexOf(" = ") > -1){
 
       var split = lines[i].split(" ");
-      var currentIndex = varNames.length;
+      var currentIndex = varAmount;
       var currentName = lines[i].substr(0, lines[i].indexOf(" = "));
 
-      for(var j = 2; j < varNames.length; j++){
+      for(var j = 0; j < varAmount; j++){
         if(currentName == varNames[j]){
           currentIndex = j;
           break;
@@ -164,7 +222,10 @@ function compile(){
 
       goToIndex(currentIndex);
 
-      varNames[currentIndex] = currentName;
+      if(currentIndex == varAmount){
+        varNames[currentIndex] = currentName;
+        varAmount++;
+      }
 
       if(split.length == 3){
 
@@ -187,7 +248,7 @@ function compile(){
             addNumber(currentIndex, parseInt(split[2], 10) + parseInt(split[4], 10));
           }
         }
-        
+
         if(split[3] == "-") {
           if(isNaN(split[2]) && isNaN(split[4])) fullSubtraction(getIndex(split[2]), getIndex(split[4]), currentIndex);
           else if (isNaN(split[2])) fullSubtNumber(getIndex(split[2]), parseInt(split[4], 10), currentIndex, false);
@@ -213,34 +274,37 @@ function compile(){
       goToIndex(currentIndex);
 
       output += ".";
-      
+
       continue;
 
     }
-    
+
     if(lines[i].indexOf(" <") > -1){
-      
+
       var currentName = lines[i].substr(0, lines[i].indexOf(" <"));
-      var currentIndex = varNames.length;
-      
-      for (var j = 2; j < varNames.length; j++) {
+      var currentIndex = varAmount;
+
+      for (var j = 0; j < varAmount; j++) {
         if (currentName == varNames[j]) {
           currentIndex = j;
           break;
         }
       }
-      
-      varNames[currentIndex] = currentName;
-      
+
+      if(currentIndex == varAmount){
+        varNames[currentIndex] = currentName;
+        varAmount++;
+      }
+
       goToIndex(currentIndex);
       output += ",";
-      
+
       continue;
-      
+
     }
 
   }
 
-  setOutput("Output: " + output);
+  setOutput(output);
 
 }
